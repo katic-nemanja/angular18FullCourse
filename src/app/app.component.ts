288fe2dc972dca1520546f19b04f92be6c852ffd
katic-nemanja <katic.nemanja@gmail.com>
2024-08-07 00:45:33 +0200

    stigao do cetvrtog poglavlja angular tutoriala

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AppNavbar } from "./navbar/navbar.component";
+import { HeaderComponent } from "./header/header.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AppNavbar, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular18FullCourse';
+  title: string = 'angular 18 FullCourse';
+  imgURL: string = 'https://image.petmd.com/files/styles/978x550/public/dog-allergies.jpg';
+  imgURL2: string = 'https://cdn.theatlantic.com/thumbor/mVxQkCl7YPBtSBVBvo0BkSOTB-w=/249x0:1749x1125/1200x900/media/img/mt/2019/03/Dr._Puppers/original.jpg';
+  isDisabled : boolean = true;
+  isActive : boolean = true;
+  fruit :string ='Apple';
 }
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -3,9 +3,12 @@ import { Component } from "@angular/core";
 @Component({
     selector:'app-navbar',
     standalone:true,
-    template: '<h1>Navbar component!!!</h1> '
+    templateUrl:'navbar.component.html',
+    styleUrl:'navbar.component.css'
 })
 
 export class AppNavbar{
-
+    city ="Veljka Vlahovica 27/10, 11550 Lazarevac";
+    author="Nemanja Katic";
+    porodica= "Natasa, Jovan i Tamara";
 }
