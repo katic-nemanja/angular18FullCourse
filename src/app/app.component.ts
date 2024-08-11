f64be2f138c2818c5c5809d81d15e47106e6ec83
katic-nemanja <katic.nemanja@gmail.com>
2024-08-12 01:23:36 +0200

    naucio ngIf i @if @else direktive, kao i ng-template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { AppNavbar } from "./navbar/navbar.component";
 import { HeaderComponent } from "./header/header.component";
 import { FormsModule } from '@angular/forms';
+import { DirectivesComponent } from "./directives/directives.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AppNavbar, HeaderComponent, FormsModule],
+  imports: [RouterOutlet, AppNavbar, HeaderComponent, FormsModule, DirectivesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/directives/directives.component.ts
@@ -0,0 +1,18 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-directives',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './directives.component.html',
+  styleUrl: './directives.component.css'
+})
+export class DirectivesComponent {
+  isLoggedIn:boolean = true;
+  userName:string = "Nemanja Katic";
+
+  kliknuto(){
+    this.isLoggedIn=!this.isLoggedIn;
+  }
+}
