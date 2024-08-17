f8956f286bf28f37ea62ab75e2e544a1a3013c62
Nemanja Katic <katic.nemanja@gmail.com>
2024-08-18 00:55:46 +0300

    part 9 tutoriala

--- src/app/directives/directives.component.ts
+++ src/app/directives/directives.component.ts
@@ -14,7 +14,20 @@ export class DirectivesComponent {
   isAdmin: boolean = false;
   isMember: boolean = false;
   isGuest: boolean = true;
+  
+  logInCounter:number = 0;
+
+  userRole:string = 'Gost u kuci';
+
+  users:Array<string> = ['Nemanja', 'Jovan', 'Tamara', 'Natasa'];
+  
   kliknuto() {
     this.isLoggedIn = !this.isLoggedIn;
+  }
+
+
+  attemptLogIn(){
+    this.logInCounter++;
+    console.log(this.logInCounter);
   }
 }
