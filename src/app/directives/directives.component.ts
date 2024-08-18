950d6ada17d5d778d27846bb14f9463bff09622a
Nemanja Katic <katic.nemanja@gmail.com>
2024-08-19 02:14:20 +0300

    stigao do 11 dela tutoriala

--- src/app/directives/directives.component.ts
+++ src/app/directives/directives.component.ts
@@ -14,20 +14,49 @@ export class DirectivesComponent {
   isAdmin: boolean = false;
   isMember: boolean = false;
   isGuest: boolean = true;
-  
-  logInCounter:number = 0;
 
-  userRole:string = 'Gost u kuci';
+  logInCounter: number = 0;
 
-  users:Array<string> = ['Nemanja', 'Jovan', 'Tamara', 'Natasa'];
-  
+  userRole: string = 'Gost u kuci';
+
+  users: Array<string> = ['Nemanja', 'Jovan', 'Tamara', 'Natasa'];
+
+  brojac:number = 1;
+
+
+  // pozivamo se na tip Array-a kao Array<any> zato sto nemamo definisan interfejs objekta,
+  // inace bi taj Array izgledao Array<ime interfejsa>, inace <any> gazi sva ta ogranicenja
+  // govoreci aplikaciji da taj objekat moze biti bilo kog tipa
+  usersObject: Array<any> = [
+    { id: 1, name: 'Nemanja Katic', email: 'Nemanja@gmail.com' },
+    { id: 2, name: 'Jovan', email: 'jovan@gmail.com' },
+    { id: 3, name: 'Natasa', email: 'natasa@gmail.com' },
+    { id: 4, name: 'Tamara', email: 'tamara@gmail.com' },
+  ];
+
   kliknuto() {
     this.isLoggedIn = !this.isLoggedIn;
   }
 
 
-  attemptLogIn(){
+  attemptLogIn() {
     this.logInCounter++;
     console.log(this.logInCounter);
+  }
+
+  dodajKorisnika(){
+    let user:object = {id:4+this.brojac, name:'Korisnik'+this.brojac, email:'korisnik'+this.brojac+'@gmail.com'};
+    this.usersObject.push(user);
+    this.brojac++;
+    }
+
+    // u ovoj funkciji prvo trazimo index elementa, pa brisemo
+  obrisiKorisnika(korisnikObject:object){
+    let index: number = this.usersObject.indexOf(korisnikObject);
+    this.usersObject.splice(index,1);
+  }
+
+  obrisiKorisnikaIndexom(index:number){
+    this.usersObject.splice(index,1);
   }
 }
