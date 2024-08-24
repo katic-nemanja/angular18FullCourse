251850ce78b91e2a564f4c17726e6da9d3fbea8c
katic-nemanja <katic.nemanja@gmail.com>
2024-08-24 18:22:43 +0200

    stigao do 16 dela tutoriala

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, AfterViewInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AppNavbar } from "./navbar/navbar.component";
 import { HeaderComponent } from "./header/header.component";
@@ -13,7 +13,9 @@ import { PostsListComponent } from "./posts-list/posts-list.component";
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+
+// implements AfterViewInit je HOOK koji se koristi da bi se nesto renderovalo tek kada se svaki VIEW ucita
+export class AppComponent implements AfterViewInit {
   title: string = 'angular 18 FullCourse';
   imgURL: string = 'https://image.petmd.com/files/styles/978x550/public/dog-allergies.jpg';
   imgURL2: string = 'https://cdn.theatlantic.com/thumbor/mVxQkCl7YPBtSBVBvo0BkSOTB-w=/249x0:1749x1125/1200x900/media/img/mt/2019/03/Dr._Puppers/original.jpg';
@@ -28,6 +30,36 @@ export class AppComponent {
   // data flow varijable
   appPostListTitle: string= 'Post broj 1';
   appIsLogin:boolean = true;
+
+  // @ViewChild(PostsListComponent) porukaIzChilda:any - ovo predstavlja deklarisanje varijable koja se vezuje za instancu
+  // Child komponente, preko koje pristupamo svim varijablama iz Childa unutar PARENTA
+  @ViewChild(PostsListComponent) childComponent:any;
+  preuzetaPorukaIzChilda:string = '';
+
+
+  constructor(){
+    console.log(this.childComponent);
+  }
+
+  // ovo je metod koji je direktno vezan za "implements AfterViewInit" i predstavlja 
+  // LIFE CYCLING HOOK koji obradjuje podatke onda kada se kompletan VIEW aplikacije ucita
+  ngAfterViewInit() {
+    console.log(this.childComponent.porukaIzChilda);
+    this.preuzetaPorukaIzChilda = this.childComponent.porukaIzChilda;
+  }
+
+  // prijem podataka iz CHILD komponente, koristeci OUTPUT dekorator
+  // pocetak
+  porukaIzChildaPrekoOutputa:string = '';
+
+  primiPoruku(message:any){
+    console.log(message);
+    this.porukaIzChildaPrekoOutputa = message;
+  }
+  // kraj
+
+  
+
   btnClick():void{
     this.isActive?this.isActive=false:this.isActive=true;
   }
--- src/app/posts-list/posts-list.component.ts
+++ src/app/posts-list/posts-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-posts-list',
@@ -12,5 +12,20 @@ export class PostsListComponent {
   @Input() postListTitle: string = '';
   @Input() postListIsLogin: boolean = false;
 
-  childMessage: string = 'Ovo je poruka generisana iz CHILD componenta';
+  porukaIzChilda: string = 'Ovo je poruka generisana iz CHILD componenta';
+
+  porukaZaParent: string = 'Ovo je poruka koja ce biti poslata parentu koristeci click Event  - @Output';
+
+  // metod za slanje podataka iz childa u Parent putem OUTPUT dekoratora
+  // porukaEvent se prosledjuje PARENTU u html fajlu kao event unutar selektora child komponente - <app-posts-list (porukaEvent)="primiPoruku($event)"...</app-posts-list>
+  // onda se tom eventu dodaje funkcija koja ce prihvatiti ono sto child emituje parentu 
+  // this.porukaEvent.emit(this.porukaZaParent); - ovo je deo zaduzen za emitovanje, sta god da mu se prosledi, emitovace
+  // pocetak
+  @Output() porukaEvent = new EventEmitter();
+
+  sendMessageToParent(){
+    console.log('Dugme je kliknuto!!');
+    this.porukaEvent.emit(this.porukaZaParent);
+  }
+  // kraj
 }
